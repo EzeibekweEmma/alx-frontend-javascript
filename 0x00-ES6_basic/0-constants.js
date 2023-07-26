40fdae0f41cc1383ce3c2bad8d633b3c6d98b641
Ezeibewke Emmanuel <ezeibekweemma@gmail.com>
2023-07-26 03:24:19 +0100

    fix code

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,16 +1,13 @@
 export function taskFirst() {
-  // using const variables
   const task = "I prefer const when I can.";
   return task;
 }
 
 export function getLast() {
-  // returning " is okay"
   return " is okay";
 }
 
 export function taskNext() {
-  // using let variables
   let combination = "But sometimes let";
   combination += getLast();
 
