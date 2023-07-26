8c5b22c02eee14ae1817493596656513cc124e1a
Ezeibewke Emmanuel <ezeibekweemma@gmail.com>
2023-07-26 15:23:26 +0100

    fix task 1 and 2

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,9 @@
 export default function taskBlock(trueOrFalse) {
-  // making use of constant variables
   const task = false;
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
+    const rtask = true;
     const task2 = false;
   }
 
